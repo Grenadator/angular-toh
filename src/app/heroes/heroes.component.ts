e952d2b96d2c47cd7b1b988e6f587a549b5e4ae7
Grenadator <grenadator@gmail.com>
2019-06-16 14:59:49 +0200

    feat: Add Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  constructor() { }
+  heroes = HEROES;
+  selectedHero: Hero;
 
-  ngOnInit() {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
